9ea3ae38642a9e152423a25340cf3fc5785c17ce
smartyshre <jadook2003@gmail.com>
2018-04-24 01:59:01 -0700

    Initial commit

---
+++ Mybrary/FBLA App/FblaLibraryApp/app/src/main/java/com/codegeek/fblalibraryapp/FirebaseId.java
@@ -0,0 +1,63 @@
+package com.codegeek.fblalibraryapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+/**
+ * Created by shreshthkharbanda for FblaLibraryApp.
+ */
+
+public class FirebaseId extends FirebaseInstanceIdService {
+    private static final String TAG = "FirebaseIDService";
+    public static final String myPrefs = "myPrefs" ;
+    public static final String tokenKey = "tokenKey";
+    public static final String registrationKey = "registrationKey";
+
+    SharedPreferences sharedpreferences;
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        sharedpreferences = getSharedPreferences(myPrefs, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString(registrationKey, refreshedToken);
+        editor.apply();
+
+        FirebaseMessaging.getInstance().subscribeToTopic("booksOverdue");
+//        Toast.makeText(getApplicationContext(), refreshedToken, Toast.LENGTH_SHORT).show();
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    /**
+     * Persist token to third-party servers.
+     *
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(String token) {
+        // Add custom implementation, as needed.
+        Log.d(TAG, "Token: " + token);
+
+        sharedpreferences = getSharedPreferences(myPrefs, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+
+        editor.putString(tokenKey, token);
+        editor.apply();
+
+        Log.v("FirebaseID--SharedPrefs", "Done saving to shared prefs!!");
+//        Toast.makeText(getApplicationContext(),"Done saving to shared prefs!!",Toast.LENGTH_LONG).show();
+    }
+}
---
+++ Mybrary/FBLA App/FblaLibraryApp/app/src/main/java/com/codegeek/fblalibraryapp/FirebaseNotifications.java
@@ -0,0 +1,105 @@
+package com.codegeek.fblalibraryapp;
+
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by shreshthkharbanda for FblaLibraryApp.
+ */
+
+public class FirebaseNotifications extends FirebaseMessagingService {
+    private static final String TAG = "FCM Service";
+    Bitmap bitmap;
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Body: " + remoteMessage.getData().get("body"));
+        Log.d(TAG, "Title: " + remoteMessage.getData().get("title"));
+        Log.d(TAG, "Icon: " + remoteMessage.getData().get("icon"));
+        Log.d(TAG, "Sound: " + remoteMessage.getData().get("sound"));
+
+        sendNotification(remoteMessage.getData().get("title"), remoteMessage.getData().get("body"), remoteMessage.getData().get("icon"));
+    }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+
+    private void sendNotification(String title, String messageBody, String iconUrl) {
+        Intent intent = new Intent(this, SwipeViewActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        try {
+            bitmap = BitmapFactory.decodeStream((InputStream) new URL(iconUrl).getContent());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String channelId = "Mybrary Channel ID";
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.mybrary_logo_16x16)
+                        .setLargeIcon(bitmap)
+                        .setContentTitle(title)
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent)
+                        .setColor(Color.argb(150, 0, 0, 0));
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        assert notificationManager != null;
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
